ca347fb31e1cb69b007cfbf71c74ae8fe4d1fad4
Edralos <titimornet@gmail.com>
2021-04-16 14:21:41 +0200

    init commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <div id="titleZone">
+      <img class="walter" src="src/assets/img/smWalrus.webp" alt="Premier Walrus" >
+      <header>The Walrus Project</header>
+      <img class="walter" src="src/assets/img/smWalrus2.webp" alt="Second walrus" >
+  </div>
+  <body>
+    
+  </body>
+  <footer>A project by Ulysse HAVE & Thibaud MORNET BLANCHET</footer>
+</div>
+  
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Description.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="desc">
+        <h1>How does it works?</h1>
+        <p id="descBig">Here you can communicate with morse code by entering text in the box below and other users on the website with a device that can
+            vibrate will receive a morse equivalent as a vibration sequence. <br>
+            On the other hand, people with compatible smartphone may tap morse code on their screen to communicate.
+        </p>
+        <p id="descSmol"> To communicate with other users, use morse code by tapping the zone below.</p>
+    </div>
+</template>
+
+<style scoped>
+    
+</style>
